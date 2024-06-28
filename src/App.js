986c6bd316b1672398fb956fdaf60e25a6a8d195
Toyosi Olaribigbe <ot.olaribigbe@outlook.com>
2024-06-28 21:29:08 +0100

    weather forecaster complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-
+import { useEffect, useState } from 'react';
 import './App.css';
+import UilReact from "@iconscout/react-unicons/icons/uil-react"
+import TopButtons from './componets/TopButtons';
+import Input from './componets/Input';
+import TandL from './componets/TandL';
+import Actualweather from './componets/Actualweather';
+import Forecasts from './componets/Forecasts';
+import formatwData from './service/weatherService';
 
 function App() {
+
+  const [query,setQuery] = useState({q: 'london'})
+  const [units, setunits] = useState('metric')
+  const [weather, setweather] = useState(null)
+
+
+
+  const fetchw = async () => {
+    const data = await formatwData({...query, units}).then( data =>
+      {setweather(data);
+
+      }
+    );
+    console.log(weather);
+  }
+  useEffect(() => {
+  
+   fetchw();
+    
+  }, [query,units])
+  
+
+ 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+   <TopButtons setQuery={setQuery}/>
+   <Input setQuery={setQuery} setunits={setunits}/>
+   { weather && (
+    <>
+    <TandL weather={weather}/>
+   <Actualweather weather={weather}/>
+   <Forecasts title="hourly forecast" data={weather.hourly}/>
+   <Forecasts title="Daily forecast" data={weather.daily}/>
+   </>
+   )
+
+   }
+   
     </div>
   );
 }
